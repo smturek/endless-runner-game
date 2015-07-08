1388aae6c7692929710c72fffea4204de2cf6d15
smturek <smturek@gmail.com>
2015-07-08 09:28:33 -0600

    added moving platforms and coins

--- js/prefabs/Platform.js
+++ js/prefabs/Platform.js
@@ -1,19 +1,84 @@
 var MrHop = MrHop || {};
 
-MrHop.Platform = function(game, numTiles, x, y) {
+MrHop.Platform = function(game, floorPool, numTiles, x, y, speed, coinPool) {
     Phaser.Group.call(this, game);
 
     this.tileSize = 40;
+    this.game = game;
+    this.enableBody = true;
+    this.floorPool = floorPool;
+    this.coinPool = coinPool;
 
-    var i = 0;
-    while(i < numTiles) {
-        var floorTile = new Phaser.Sprite(game, x + i * this.tileSize, y, 'floor');
-
-        this.add(floorTile);
-
-        i++
-    }
-}
+    this.prepare(numTiles, x, y, speed);
+};
 
 MrHop.Platform.prototype = Object.create(Phaser.Group.prototype);
 MrHop.Platform.prototype.constructor = MrHop.Platform;
+
+MrHop.Platform.prototype.prepare = function(numTiles, x, y, speed) {
+
+    this.alive = true;
+
+    var i = 0;
+    while(i < numTiles) {
+
+        var floorTile = this.floorPool.getFirstExists(false);
+
+        if(!floorTile) {
+            floorTile = new Phaser.Sprite(this.game, x + i * this.tileSize, y, 'floor');
+        }
+        else {
+            floorTile.reset(x + i * this.tileSize, y);
+        }
+
+        this.add(floorTile);
+
+        i++;
+    }
+
+    this.setAll('body.immovable', true);
+    this.setAll('body.allowGravity', false);
+    this.setAll('body.velocity.x', speed);
+
+    this.addCoins(speed);
+
+};
+
+MrHop.Platform.prototype.kill = function() {
+    this.alive = false;
+    this.callAll('kill');
+
+    var sprites = [];
+
+    this.forEach(function(tile){
+        sprites.push(tile);
+    }, this);
+
+    sprites.forEach(function(tile){
+        this.floorPool.add(tile);
+    }, this);
+}
+
+MrHop.Platform.prototype.addCoins = function (speed) {
+    var coinsY = 90 + Math.random() * 110;
+    var hasCoin;
+
+    this.forEach(function(tile){
+        hasCoin = Math.random() <= 0.4;
+
+        if(hasCoin) {
+            var coin = this.coinPool.getFirstExists(false);
+
+            if(!coin) {
+                coin = new Phaser.Sprite(this.game, tile.x, tile.y - coinsY, 'coin');
+                this.coinPool.add(coin);
+            }
+            else {
+                coin.reset(tile.x, tile.y - coinsY)
+            }
+
+            coin.body.velocity.x = speed;
+            coin.body.allowGravity = false;
+        }
+    }, this);
+};
--- js/states/Game.js
+++ js/states/Game.js
@@ -2,13 +2,136 @@ var mrHop = MrHop || {};
 
 MrHop.GameState = {
     init: function() {
+
+        this.floorPool = this.add.group();
+
+        this.platformPool = this.add.group();
+
+        this.coinPool = this.add.group();
+        this.coinPool.enableBody = true;
+
         this.game.physics.arcade.gravity.y = 1000;
+
+        this.maxJumpDistance = 120;
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+
+        this.myCoins = 0;
+
+        this.levelSpeed = 200;
     },
     create: function() {
-        var platform = new MrHop.Platform(this.game, 3, 100, 100);
-        this.add.existing(platform);
+
+        //create the player
+        this.player = this.add.sprite(50, 50, 'player');
+        this.player.anchor.setTo(0.5);
+        this.player.animations.add('running', [0, 1, 2, 3, 2, 1], 15, true);
+        this.game.physics.arcade.enable(this.player);
+
+        //change the player bounding box
+        this.player.body.setSize(38, 60, 0, 0);
+        this.player.play('running');
+
+        this.currentPlatform = new MrHop.Platform(this.game, this.floorPool, 12, 0, 100, -this.levelSpeed, this.coinPool);
+        this.platformPool.add(this.currentPlatform);
+
+        this.loadLevel();
     },
     update: function() {
 
+        this.platformPool.forEachAlive(function(platform, index) {
+            this.game.physics.arcade.collide(this.player, platform);
+
+            if(platform.length && this.currentPlatform.children[this.currentPlatform.length - 1].right < 0) {
+                platform.kill();
+            }
+        }, this);
+
+        this.coinPool.forEachAlive(function(coin) {
+            if(coin.right <= 0) {
+                coin.kill();
+            }
+        }, this);
+
+        if(this.player.body.touching.down) {
+            this.player.body.velocity.x = this.levelSpeed;
+        }
+        else {
+            this.player.body.velocity.x = 0;
+        }
+
+        if(this.cursors.up.isDown || this.game.input.activePointer.isDown) {
+            this.playerJump();
+        }
+        else if(this.cursors.up.isUp || this.game.input.activePointer.isUp) {
+            this.isJumping = false;
+        }
+
+        if(this.currentPlatform.length && this.currentPlatform.children[this.currentPlatform.length - 1].right < this.game.world.width) {
+            this.createPlatform();
+        }
+    },
+    playerJump: function() {
+        if(this.player.body.touching.down) {
+            this.startJumpY = this.player.y;
+            this.isJumping = true;
+            this.jumpPeaked = false;
+
+            this.player.body.velocity.y = -300;
+        }
+        else if(this.isJumping && !this.jumpPeaked) {
+            var distanceJumped = this.startJumpY - this.player.y;
+
+            if(distanceJumped <= this.maxJumpDistance) {
+                this.player.body.velocity.y = -300;
+            }
+            else {
+                this.jumpPeaked = true;
+            }
+        }
+    },
+    loadLevel: function() {
+        this.createPlatform();
+    },
+    createPlatform: function() {
+        var nextPlatformData = this.generatePlatform();
+
+        if(nextPlatformData) {
+
+            this.currentPlatform = this.platformPool.getFirstDead();
+
+            if(!this.currentPlatform) {
+                this.currentPlatform = new MrHop.Platform(this.game, this.floorPool, nextPlatformData.numTiles, this.game.world.width + nextPlatformData.separation, nextPlatformData.y, -this.levelSpeed, this.coinPool);
+            }
+            else {
+                this.currentPlatform.prepare(nextPlatformData.numTiles, this.game.world.width + nextPlatformData.separation, nextPlatformData.y, -this.levelSpeed);
+            }
+
+            this.platformPool.add(this.currentPlatform);
+
+        }
+    },
+    generatePlatform: function() {
+        var data = {};
+
+        var minSeparation = 60;
+        var maxSeparation = 200;
+        data.separation = minSeparation + Math.random() * (maxSeparation - minSeparation);
+
+        var minDiffY = -120;
+        var maxDiffY = 120;
+        data.y = this.currentPlatform.children[0].y + minDiffY + Math.random() * (maxDiffY - minDiffY);
+        data.y = Math.max(150, data.y);
+        data.y = Math.min(this.game.world.height - 50, data.y);
+
+        var minTiles = 1;
+        var maxTiles = 5;
+        data.numTiles = minTiles + Math.random() * (maxTiles - minTiles);
+
+        return data;
     }
+    // render: function() {
+    //     this.game.debug.body(this.player);
+    //     this.game.debug.bodyInfo(this.player, 0, 30);
+    // }
 };
